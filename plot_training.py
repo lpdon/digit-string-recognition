be576db14d1f4b6bdde5af2d071324667ea743b9
Lucas Steinmann <lucas@steinm.net>
2018-12-31 09:02:56 +0100

    Add option to plot every column in a separate plot.

--- plot_training.py
+++ plot_training.py
@@ -1,4 +1,5 @@
 import csv
+import math
 from argparse import ArgumentParser
 from typing import List, Any, Dict
 
@@ -10,6 +11,8 @@ def parse_args():
     parser.add_argument("--log", required=False, type=str, help="Path to the log file.")
     parser.add_argument("--columns", required=True, nargs='+', type=str,
                         help="Specifies which values should be plotted")
+    parser.add_argument('--multi', default=False, action='store_true',
+                        help='If true, plot every column into a separate plot.')
     return parser.parse_args()
 
 
@@ -20,15 +23,21 @@ def read_log(log_file: str) -> List[Dict[str, Any]]:
     return history
 
 
-def plot(log_file: str, columns: List[str]):
+def plot(log_file: str, columns: List[str], multiple_plots: bool = False):
     history = read_log(log_file)
     selected_columns = [[row[sel_key] for row in history] for sel_key in columns]
-    for col_name, col in zip(columns, selected_columns):
+    num_plot_rows = int(math.ceil(math.sqrt(len(columns)))) if multiple_plots else 1
+    print(num_plot_rows)
+
+    for num, (col_name, col) in enumerate(zip(columns, selected_columns)):
+        # Find the right spot on the plot
+        if multiple_plots:
+            plt.subplot(num_plot_rows, num_plot_rows, num + 1)
         plt.plot(col, label=col_name)
-    plt.legend()
+        plt.legend()
     plt.show()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    plot(args.log, columns=args.columns)
+    plot(args.log, columns=args.columns, multiple_plots=args.multi)
